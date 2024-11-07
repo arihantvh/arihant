2931710a8c39547069254249baf1185612669f7d
arihantvh <arihantv9339@gmail.com>
2024-11-07 16:20:49 +0530

    Add files via upload

---
+++ ArrayMul.java
@@ -0,0 +1,31 @@
+class ArrayMul{
+
+  public static void main(String[] args)
+
+ {
+
+  int a1[] ={2,5,-2,10};
+
+  int a2[]={3,-5,7,1};
+
+  int len=4;
+
+
+  for (int=0;i<len;i++)
+  int result[]=new int[len];
+
+
+  
+
+  {
+
+  //multipling corresponding element
+
+  result[i]=a1[i]*a2[i];
+
+  System.out.println(result[i]+"");
+ }
+
+
+ }
+}
